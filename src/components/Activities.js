99b053d46a23b99013c745103f33480f8f128ae7
Stuti <stutib5304@gmail.com>
2023-07-05 12:26:24 +0530

    added activities

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,9 +1,27 @@
 import React from 'react'
 const Activities=()=>{
+    
     return(
-        <div className='activities'>
-<h1>Activities page</h1>
-</div>
-    )
-}
-export default Activities
+       <div className='content'>
+         
+        <div className="text-content">
+          <h1 className='activities-head'>Activities</h1>
+          <p><br></br></p>
+          <p>
+          The Counselling center is involved in student life at the campus through different levels of interactive processes that are all over seen and directed by the counsellor herself.</p>
+<br></br>
+<p>The Counselling center’s mental health activities focus on early Identification & intervention, as well as prevention of mental health problems of students. Individual counselling sessions are conducted by the counsellor for the students who seek help on their own or are referred. The referrals usually come from Institute Medical officers, and Academic office .The volunteers and Gymkhana members also share mental health concerns of their peers with the counsellor.</p>
+<br></br>
+<p>For new students and parents the orientation programme itself is the time where the counselling center is introduced and its various functions highlighted by the counsellor. Counsellor herself observes and interacts with new students in the institute and hostel campus in formal and informal settings by visiting them during their labs, tutorials, free slots etc.</p>
+<br></br>
+<p>The student volunteers of the center and student Gymkhana members also help freshers by extending their helping hand both for academic and non-academic concerns. The center facilitates the peer tutoring classes where students help and each other with academic difficulties in topics.
+          </p><br></br>
+          <p>
+            <a href="https://www.iiti.ac.in/public/storage/events/October2022/events%20complete.pdf">Events Gallery</a>
+            <br></br>
+          </p>
+        </div>
+      </div>
+    );
+};
+export default Activities;
