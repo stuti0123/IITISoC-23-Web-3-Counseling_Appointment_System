743c75772b2199754a81f95f19d67287b022ddb5
Anubhuti214 <anubhutis214@gmail.com>
2023-07-08 12:47:15 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import './App.css';
-import Heading from './components/Heading'
+import './Login.css';
+import Heading from './components/Heading';
 import Homepg from './components/Homepg';
 import Navbar from './components/Navbar';
 import BackToTopButton from './components/BackToTopButton';
 import Footer from './components/Footer';
-import { BrowserRouter,React,Reacts, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Activities from './components/Activities';
 import Information from './components/Information';
+
 function App() {
   return (
     <div className="App">
-        <BackToTopButton/>
-        <Heading/>
-        <BrowserRouter>
-        <Navbar/>
+      <BackToTopButton />
+      <Heading />
+      <Router>
+        <Navbar />
         <Routes>
-            <Route exact path="/" element={ <Homepg/>}/>
-            <Route  exact path="/activities" element={ <Activities/>}/>
-            <Route  exact path="/information" element={ <Information/>}/>
+          <Route exact path="/" element={<Homepg />} />
+          <Route exact path="/activities" element={<Activities />} />
+          <Route exact path="/information" element={<Information />} />
+          <Route exact path="/login" element={<Homepg />} />
         </Routes>
-        </BrowserRouter>
-        <Footer/>
+      </Router>
+      <Footer />
     </div>
   );
 }
